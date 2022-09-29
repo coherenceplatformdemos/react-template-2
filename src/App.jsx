8d7063c5a1d5f7baf780597af20c1a3ab3d3496a
Robert Guss <robertguss@pm.me>
2022-09-29 11:30:27 -0400

    removed comments from home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,3 @@
-/*
-  This example requires Tailwind CSS v2.0+
-
-  This example requires some changes to your config:
-
-  ```
-  // tailwind.config.js
-  const colors = require('tailwindcss/colors')
-
-  module.exports = {
-    // ...
-    theme: {
-      extend: {
-        colors: {
-          rose: colors.rose,
-        },
-      },
-    },
-    plugins: [
-      // ...
-      require('@tailwindcss/forms'),
-    ],
-  }
-  ```
-*/
 import { ChevronRightIcon, StarIcon } from "@heroicons/react/20/solid";
 
 const stats = [
